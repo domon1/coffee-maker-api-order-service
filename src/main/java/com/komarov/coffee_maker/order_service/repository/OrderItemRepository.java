d23b724e17f7f670d90d70499a58158a7faec87d
domon1 <dimoniy.dota2@mail.ru>
2025-05-17 19:46:40 +0300

    feat: Добавление репозиториев

---
+++ src/main/java/com/komarov/coffee_maker/order_service/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.komarov.coffee_maker.order_service.repository;
+
+import com.komarov.coffee_maker.order_service.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
---
+++ src/main/java/com/komarov/coffee_maker/order_service/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.komarov.coffee_maker.order_service.repository;
+
+import com.komarov.coffee_maker.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
